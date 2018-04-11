8fc770838bed1c6a0d65bdddb29ea92b5a850f43
Harry Frank <harry.frank@1901group.com>
2018-04-11 11:07:49 -0400

    Added new controller servlet to demo EJB injection.

---
+++ AirlineWeb/src/com/airline/controllers/FlightDetails.java
@@ -0,0 +1,62 @@
+package com.airline.controllers;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.ejb.EJB;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.airline.service.FlightService;
+
+/**
+ * Servlet implementation class FlightDetails
+ */
+@WebServlet("/FlightDetails")
+public class FlightDetails extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	// private FlightService fs = new FlightService();
+	// We don't do this because it will just get a new object, but will strip out
+	// all the EJB features. So, we use annotations to inject.
+	
+	@EJB
+	private FlightService fs;
+	//This asks the container to give us an object from its "pool" of bean objects.
+	
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public FlightDetails() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		
+		PrintWriter out = response.getWriter();
+		out.println("The flight details servlet has been called!");
+		out.println(fs.getAirplaneModel());
+		out.println(fs.getFrom());
+		out.println(fs.getTo());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
